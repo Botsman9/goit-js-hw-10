f5a4675dfdb58902932fc9b72dce315f89d05f74
Dmitriy <dmitriy.botsman@gmail.com>
2021-11-19 22:47:59 +0200

    first

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
-import './css/styles.css';
+// import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
 
 const searchBoxRef = document.querySelector('#search-box');
 const countryListRef = document.querySelector('.country-list');
 const countryInfoRef = document.querySelector('.country-info');
+
+const BASE_URL = 'https://restcountries.com/v3.1/name/';
+
+fetchCountries = url => {
+  return fetch(BASE_URL + url).then(response => response.json());
+};
+// fetchCountries('/peru').then(data => console.log(data.flatMap(rrr => rrr.name.common)));
+
+searchBoxRef.addEventListener('input', e => {
+  e.preventDefault;
+
+  fetchCountries('e').then(
+    data => (countryListRef.textContent = data.flatMap(rrr => rrr.name.common)),
+  );
+});
+
+// countryListRef;
