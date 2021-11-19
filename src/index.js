14d90c68c79a84d7ddf6a19212b2f300cf9e9674
Dmitriy <dmitriy.botsman@gmail.com>
2021-11-19 16:10:23 +0200

    name

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const searchBoxRef = document.querySelector('#search-box');
+const countryListRef = document.querySelector('.country-list');
+const countryInfoRef = document.querySelector('.country-info');
